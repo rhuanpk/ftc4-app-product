9f3559fe75499be58a4f2152cb7cdf14e890a228
Diógenes Viana <65191714+diogenesviana@users.noreply.github.com>
2024-11-16 12:54:39 -0300

    test: testes do usecases.

---
+++ src/test/java/org/example/product/core/applications/produto/usecases/AtualizarProdutoTest.java
@@ -0,0 +1,51 @@
+package org.example.product.core.applications.produto.usecases;
+
+import org.example.product.application.driven.entities.produto.ProdutoEntity;
+import org.example.product.core.applications.produto.repositories.ProdutoRepositoryInterface;
+import org.example.product.core.domain.produto.enums.TipoProduto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@SpringBootTest
+class AtualizarProdutoTest {
+
+    private AtualizarProduto atualizarProduto;
+
+    @Mock
+    private ProdutoRepositoryInterface produtoRepositoryInterface;
+
+    private ProdutoEntity produtoEntity;
+
+    @Test
+    void whenExecuteUpdateProductThenUpdateProduct() {
+        atualizarProduto.execute(produtoEntity.getId(), produtoEntity.getNome(), produtoEntity.getPreco(), produtoEntity.getTipoProduto());
+
+        Mockito.verify(produtoRepositoryInterface, Mockito.times(1))
+                .update(produtoEntity.getId(), produtoEntity.getNome(), produtoEntity.getPreco(), produtoEntity.getTipoProduto());
+    }
+
+    @BeforeEach
+    void setUp() {
+
+        MockitoAnnotations.openMocks(this);
+        atualizarProduto = new AtualizarProduto(produtoRepositoryInterface);
+
+
+        produtoEntity = new ProdutoEntity();
+        UUID id = UUID.fromString("838b1e25-a271-4816-a16b-a585470d84ed");
+        produtoEntity.setId(id);
+        produtoEntity.setNome("Produto 1");
+        produtoEntity.setPreco(BigDecimal.TEN);
+        produtoEntity.setTipoProduto(TipoProduto.BEBIDA);
+
+    }
+
+}
---
+++ src/test/java/org/example/product/core/applications/produto/usecases/DeletarProdutoTest.java
@@ -0,0 +1,41 @@
+package org.example.product.core.applications.produto.usecases;
+
+import org.example.product.core.applications.produto.repositories.ProdutoRepositoryInterface;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DeletarProdutoTest {
+
+    private DeletarProduto deletarProduto;
+
+    @Mock
+    private ProdutoRepositoryInterface produtoRepositoryInterface;
+
+
+    @Test
+    void whenExecuteDeleteProductThenDeleteProduct() {
+
+        UUID id = UUID.fromString("838b1e25-a271-4816-a16b-a585470d84ed");
+        deletarProduto.execute(id);
+
+        Mockito.verify(produtoRepositoryInterface, Mockito.times(1)).deleteProdutoById(id);
+    }
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        deletarProduto = new DeletarProduto(produtoRepositoryInterface);
+    }
+
+}
