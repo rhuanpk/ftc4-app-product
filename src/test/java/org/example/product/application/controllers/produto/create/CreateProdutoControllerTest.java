12f01bfc015861bbcc326fa956ee85921f1d06f3
Diógenes Viana <65191714+diogenesviana@users.noreply.github.com>
2024-11-16 15:04:58 -0300

    test: Ajustando teste.

--- src/test/java/org/example/product/application/controllers/produto/create/CreateProdutoControllerTest.java
+++ src/test/java/org/example/product/application/controllers/produto/create/CreateProdutoControllerTest.java
@@ -1,21 +1,15 @@
 package org.example.product.application.controllers.produto.create;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.product.adapters.controllers.ProdutoController;
 import org.example.product.adapters.presenters.ProdutoPresenter;
 import org.example.product.core.applications.produto.repositories.ProdutoRepositoryInterface;
 import org.example.product.core.domain.produto.Produto;
 import org.example.product.core.domain.produto.enums.TipoProduto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,20 +20,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mockStatic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+
+@SpringBootTest
 class CreateProdutoControllerTest {
 
     @Mock
     private ProdutoRepositoryInterface produtoRepositoryInterface;
-
-    @InjectMocks
-    private CreateProdutoController createProdutoController;
-
-    private ProdutoController produtoController;
 
     private MockMvc mockMvc;
 
@@ -47,48 +38,29 @@ class CreateProdutoControllerTest {
 
     @Test
     void create() throws Exception {
-        // Criando o produto para o teste
         Produto produto = new Produto(UUID.randomUUID(), "Produto 1", BigDecimal.TEN, TipoProduto.BEBIDA);
 
-        // Criando o mock de resposta que será retornado pelo método estático
         Map<String, Object> mockResponse = new HashMap<>();
         mockResponse.put("id", produto.getId());
         mockResponse.put("nome", produto.getNome());
         mockResponse.put("preco", produto.getPreco());
         mockResponse.put("tipo_produto", produto.getTipoProduto());
 
-        // Mockando o método estático ProdutoPresenter.toObject()
         try (MockedStatic<ProdutoPresenter> mockedStatic = mockStatic(ProdutoPresenter.class)) {
-            // Quando ProdutoPresenter.toObject(produto) for chamado, retornar o mockResponse
             mockedStatic.when(() -> ProdutoPresenter.toObject(any(Produto.class))).thenReturn(mockResponse);
 
-            // Mockando o método salvar() para garantir que o comportamento do controller seja mockado
-            when(produtoController.salvar(produto.getNome(), produto.getPreco(), produto.getTipoProduto()))
-                    .thenReturn(mockResponse);
-
-            // Verificando o estado do produto antes de chamar ProdutoPresenter.toObject
-            Produto produtoTest = new Produto(produto.getId(), produto.getNome(), produto.getPreco(), produto.getTipoProduto());
-            System.out.println("Produto antes de chamar toObject: " + produtoTest);
-
-            // Realizando o POST e verificando apenas o status HTTP
             mockMvc.perform(post("/produtos")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsString(produto)))
-                    .andExpect(status().isCreated()); // Validando apenas o status HTTP
+                    .andExpect(status().isCreated());
         }
-
     }
-
 
     @BeforeEach
     void setUp() {
-        // Inicializando os mocks
         MockitoAnnotations.openMocks(this);
-
-        // Criando o ProdutoController com a dependência mockada
-        produtoController = new ProdutoController(produtoRepositoryInterface);
-
-        // Configurando o MockMvc para testar o controller
+        CreateProdutoController createProdutoController = new CreateProdutoController(produtoRepositoryInterface);
         this.mockMvc = MockMvcBuilders.standaloneSetup(createProdutoController).build();
+
     }
 }
